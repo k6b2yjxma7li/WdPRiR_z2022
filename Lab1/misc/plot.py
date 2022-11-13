45dd4cf31ec6acbd03fe7ef2887b629a776d0a92
k6b2yjxma7li <rkrasala@gmail.com>
2022-11-13 17:28:32 +0100

    Feature: plotting script (+results) (#2)
    * Added plotting tool (Python)
    
    * Added generation results
    - Mandelbrot set picture 8192x8192
    - log-log plot of generation time from 2^5 to 2^13 pixels (squares)
    
    Co-authored-by: k6b2yjxma7li <01110164@pw.edu.pl>

---
+++ Lab1/misc/plot.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+results = {}
+
+with open("./timings.csv", "r") as ifile:
+    while True:
+        line = ifile.readline()
+        if not line:
+            break   
+        fields = line.split(",")
+        results[int(fields[0])] = float(fields[1][:-1])
+plt.plot(results.keys(), results.values())
+
+plt.xscale('log')
+plt.yscale('log')
+
+plt.grid(b=True, which='both', lw=0.3)
+
+plt.xlabel("Square image edge size [px]")
+plt.ylabel("Execution time [s]")
+
+plt.savefig("timings.png")
